3e2180f9dc409c5e07a3523ee1bd9bd820220cd8
alana <alanarust@gmail.com>
2021-01-20 22:01:37 -0800

    added diceRoll func

--- dailydiceroll.py
+++ dailydiceroll.py
@@ -3,5 +3,11 @@
 # date: 01-20-2021
 
 import random
-num = random.randint(1, 6)
-print('Your roll is : ' + str(num))
+def diceRoll():
+    playDice = int(input('To play the Daily Dice Roll, press 1 or press 0 to end'))
+    while playDice == 1 | playDice == 0
+        if playDice == 1:
+            num = random.randint(1, 6)
+            print('Your roll is : ' + str(num))
+        else:
+            exit()
